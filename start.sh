9af4de5f8003f7266e857c23e944132802f896c8
Robustus143 <perfekcionist1453@gmail.com>
2025-12-13 17:19:25 +0100

    add debug logging to start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+set -e
+echo "Migrating database..."
 python manage.py migrate
+echo "Loading initial data..."
 python manage.py loaddata initial_data.json
+echo "Starting Gunicorn..."
 gunicorn mysite.wsgi:application --bind 0.0.0.0:$PORT
